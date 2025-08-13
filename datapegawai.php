e9a89a42751cd03d66ecb9853b1a4758d88e6a76
Agizz-svg <Argies2007>
2025-08-13 09:57:15 +0700

    datapegawai commit

---
+++ datapegawai.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Membuat login Dengan PHP dan MySQLi - www.malasngoding.com</title>
+    <link rel="stylesheet" type="text/css" href="datapegawai.css">
+</head>
+<body>
+    <h2>WWW.MALASNGODING.COM</h2>
+
+    <div class="kotak">
+        
+        <h3>Data Pegawai</h3>
+
+        <div>
+            
+            <label>Cari Pegawai</label>
+            <br>
+            <input type="text" class="cari" name="cari" placeholder="cari pegawai di sini..">
+            <div class="tableku"></div>
+        </div>
+
+</br>
+
+       
+    </div>
+</body>
+<script src="jquery-3.7.1.min.js"></script>
+<script src="datapegawai.js"></script>
+</html>
